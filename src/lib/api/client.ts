a8a65271649fa339754b04c3586933ff4114070e
afushimi-source <tanafushi@gmail.com>
2021-12-14 21:57:21 +0900

    fix client url

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -8,8 +8,7 @@ const options = {
 const client = applyCaseMiddleware(
   axios.create({
     // baseURL: "http://localhost:3001/api/v1",
-    baseURL:
-      "http://timer-external-alb-1532364907.ap-northeast-1.elb.amazonaws.com//api/v1",
+    baseURL: process.env.REACT_APP_API_URL,
   }),
   options,
 );
